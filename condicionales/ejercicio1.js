729000b94c4c54e77ae55c1e46d690cb73f09246
Formasster <amarilishenit@gmail.com>
2025-05-16 12:34:04 +0200

    final

---
+++ condicionales/ejercicio1.js
@@ -0,0 +1,8 @@
+//🗳 Ejercicio 1: ¿Eres mayor para votar o aún ves caricaturas?
+
+let edad = parseInt(prompt("Hey tú, ¿cuántos años tienes? (Sé honesto 😎)"));
+if (edad >= 18) {
+alert("¡Felicidades! Puedes votar y quejarte legalmente de los políticos 🗳");
+} else {
+alert("Todavía no puedes votar... pero al menos puedes ver dibujos animados sin culpa 🍿");
+}
